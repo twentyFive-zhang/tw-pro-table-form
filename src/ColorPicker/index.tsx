8b2a9ea8aad8295f191af0316bfbdd07977e7662
二五 <1097983662@qq.com>
2023-04-23 02:46:24 +0800

    fix: test

--- src/ColorPicker/index.tsx
+++ src/ColorPicker/index.tsx
@@ -1,5 +1,7 @@
 import React, { type FC } from 'react';
 
-const ColorPicker: FC<{ title: string }> = (props) => <h4>{props.title}</h4>;
+const ColorPicker: FC<{ title: string }> = (props) => (
+  <h4>{props.title}test</h4>
+);
 
 export default ColorPicker;
